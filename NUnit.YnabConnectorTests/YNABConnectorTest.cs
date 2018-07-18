b1e60beb3db05b5d95c75de5292c7e756e9e5b6b
Дмитрий <faithfinder@yandex.ru>
2018-07-18 16:02:11 +0300

    Started developing connector to YNAB and a test suite

---
+++ NUnit.YnabConnectorTests/YNABConnectorTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using YNABConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace NUnit.YnabConnectorTests
+{
+    [TestFixture]
+    public class YNABConnectorTest
+    {
+        public YNABConnectorTest()
+        {
+            AccessToken = Resources.AccessToken;
+        }
+
+        private readonly string AccessToken;
+
+        [Test]
+        public void GetBudgetTest()
+        {
+            var ynabClient = new YNABClient(AccessToken);
+            var budget = ynabClient.GetBudgets();
+            Debug.WriteLine(budget);
+            Assert.That(budget is string);
+        }
+    }
+}
---
+++ YNABConnector/YNABClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace YNABConnector
+{
+    public class YNABClient
+    {
+
+        public string AccessToken { get; }
+        private HttpClient client;
+
+        public YNABClient(string accessToken)
+        {
+            AccessToken = accessToken;
+            client = new HttpClient();
+        }
+
+        public string GetBudgets()
+        {
+            client.BaseAddress = new Uri("https://api.youneedabudget.com/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", AccessToken);
+
+            HttpResponseMessage response = client.GetAsync("v1/budgets").Result;
+            var result = "";
+            if (response.IsSuccessStatusCode)
+            {
+                result = response.Content.ReadAsStringAsync().Result;
+            }
+            return result;
+        }
+    }
+}

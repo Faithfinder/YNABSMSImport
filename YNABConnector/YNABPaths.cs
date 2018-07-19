75a0d7042a6e709d54faf29d10d6b4c4723ca5de
Дмитрий <faithfinder@yandex.ru>
2018-07-19 17:20:29 +0300

    API paths to static class

---
+++ YNABConnector/YNABPaths.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace YNABConnector
+{
+    internal static class YNABPaths
+    {
+        internal const string Budgets = "v1/budgets";
+    }
+}

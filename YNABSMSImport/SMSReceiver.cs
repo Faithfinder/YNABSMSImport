6f86cdcffb34e5659441b0cbcc1f90df65b24161
Дмитрий <faithfinder@yandex.ru>
2018-08-14 12:00:39 +0300

    Multipart sms processing

--- YNABSMSImport/SMSReceiver.cs
+++ YNABSMSImport/SMSReceiver.cs
@@ -1,4 +1,6 @@
-﻿using Android.App;
+﻿using System.Collections.Generic;
+
+using Android.App;
 using Android.Content;
 using Android.Provider;
 using Android.Telephony;
@@ -16,17 +18,37 @@ namespace YNABSMSImport
             if (!intent.HasExtra("pdus")) return;
 
             var messages = Telephony.Sms.Intents.GetMessagesFromIntent(intent);
+            var gluedMessages = GlueMessagesTogether(messages);
 
-            foreach (var message in messages) StartProcessing(context, message);
+            foreach (var message in gluedMessages) StartProcessing(context, message);
         }
 
-        private static void StartProcessing(Context context, SmsMessage message)
+        private static void StartProcessing(Context context, KeyValuePair<string,string> message)
         {
             var smsProcessor = new Intent(context, typeof(SMSProcessor));
-            smsProcessor.PutExtra("Address", message.DisplayOriginatingAddress);
-            smsProcessor.PutExtra("Message", message.DisplayMessageBody);
+            smsProcessor.PutExtra("Address", message.Key);
+            smsProcessor.PutExtra("Message", message.Value);
 
             context.StartService(smsProcessor);
+        }
+
+        private static Dictionary<string, string> GlueMessagesTogether(IEnumerable<SmsMessage> messages)
+        {
+            var result = new Dictionary<string,string>();
+
+            foreach (var smsMessage in messages)
+            {
+                if (!result.ContainsKey(smsMessage.DisplayOriginatingAddress))
+                {
+                    result.Add(smsMessage.DisplayOriginatingAddress, smsMessage.DisplayMessageBody);
+                }
+                else
+                {
+                    result[smsMessage.DisplayOriginatingAddress] += smsMessage.DisplayMessageBody;
+                }
+            }
+
+            return result;
         }
     }
 }
